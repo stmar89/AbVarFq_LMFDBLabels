a24f2fd73cbaf7225bbabe17007ff0ba5900bfce
Stefano Marseglia <stefano.marseglia89@gmail.com>
2025-06-20 04:00:18 -0400

    added printf when increasing prec in script_cm

--- computation/script_cm.m
+++ computation/script_cm.m
@@ -13,15 +13,31 @@
     try 
         P<x>:=PolynomialRing(Integers());
         f:=P!eval(s);
-        A:=EtaleAlgebra(f);
-        g,q:=DimensionSizeFiniteField(A);
-        PHI:=pAdicPosCMType(A);
-        str:=SaveCMType(PHI);
-
         label:=IsogenyLabel(f);
         file_out:=fld_out cat label cat "_cm.txt";
-        fprintf file_out,"%o",str;
-        printf "%o: done\n",label;
+        if FileAlreadyExists(file_out) then
+            quit;
+        end if;
+
+        A:=EtaleAlgebra(f);
+        g,q:=DimensionSizeFiniteField(A);
+        k:=30;
+        go:=false;
+        while not go and k lt 10^3 do
+            try
+                PHI:=pAdicPosCMType(A : precpAdic:=k);
+                str:=SaveCMType(PHI);
+                fprintf file_out,"%o",str;
+                printf "%o: done\n",label;
+                go:=true;
+            catch e
+                k+:=100;
+                printf "increasing precision to %o for %o\n",k,label;
+            end try;
+        end while;
+        if k ge 10^3 then
+            quit;
+        end if;
     catch e
         printf "*********************************************\n%o\n%o\n", label,e;
         fprintf issues, "*********************************************\n%o\n%o\n", label,e;
