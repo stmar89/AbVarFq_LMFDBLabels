310af2aedc9276a15c1638083b898cd9cb419afa
Stefano Marseglia <stefano.marseglia89@gmail.com>
2025-09-02 16:07:57 +0000

    restarted computation of missing ppols on babbage

--- computation/script_missing_ppols.m
+++ computation/script_missing_ppols.m
@@ -1,44 +1,22 @@
 /* vim: set syntax=magma : */
-/*
-    variables:
-        issues       # we collect here the issue from the weak eq script
-        fld_out      # output folder
-        s = string of integers, eg [ 101, -20, 1 ] 
-*/
-   
-    done:=Split(Pipe("ls /data/stmar/287_abvarfq_lmfdb_recomputation/output_wk/","r");
-    CONTINUE FROM HERE
-        f:=P!eval(s);
-        label:=IsogenyLabel(f);
-        file_out:=fld_out cat label cat "_cm.txt";
-        if OpenTest(file_out,"r") then
-            printf "%o already done\n",label;
-            quit;
-        end if;
+  
+    
+    done:=Split(Pipe("ls /data/stmar/287_abvarfq_lmfdb_recomputation/output_pols/av_fq_isog/","r"));
+    input:=Split(Read("~/AbVarFq_LMFDBLabels/computation/weil_poly_sqfree_ord_cs_no_3_25.txt")) cat 
+           Split(Read("~/AbVarFq_LMFDBLabels/computation/weil_poly_sqfree_ord_only_3_25.txt"));
+    file_out:="~/AbVarFq_LMFDBLabels/computation/weil_poly_missing_pp_script_with_3_25.txt";
 
-        A:=EtaleAlgebra(f);
-        g,q:=DimensionSizeFiniteField(A);
-        k:=30;
-        go:=false;
-        while not go and k lt 10^3 do
-            try
-                PHI:=pAdicPosCMType(A : precpAdic:=k);
-                str:=SaveCMType(PHI);
-                fprintf file_out,"%o",str;
-                printf "%o: done\n",label;
-                go:=true;
-            catch e
-                k+:=30;
-                e;
-                printf "increasing precision to %o for %o\n",k,label;
-            end try;
-        end while;
-        if k ge 10^3 then
-            quit;
-        end if;
-    catch e
-        printf "*********************************************\n%o\n%o\n", label,e;
-        fprintf issues, "*********************************************\n%o\n%o\n", label,e;
-    end try;
+    AttachSpec("~/AbVarFq_LMFDBLabels/spec");
+    PP<x>:=PolynomialRing(Integers());
+    input_labels:=[ IsogenyLabel(PP!eval(s)) : s in input ];
+    missing_labels:=Seqset(input_labels) diff Seqset(done);
+    missing_coeffs:=[ Coefficients(f) where _,_,f:=LabelToPoly(ll) : ll in missing_labels ];
+    printf "total input = %o\tdone = %o\tmissing = %o\n",#input,#done,#missing_coeffs;
+
+    "printing";
+    for cc in missing_coeffs do
+        fprintf file_out,"%o\n",cc;
+    end for;
+
     quit;
 
--- computation/script_separate_ppol_from_nppol.m
+++ computation/script_separate_ppol_from_nppol.m
@@ -16,26 +16,48 @@
     // fld_dest_pp:="~/287_abvarfq_lmfdb_recomputation/output_pols/av_fq_pol/";
     fld_old_code:="/data/stmar/287_abvarfq_lmfdb_recomputation/output_after_broken_rsync/av_fq_pol/";
     fld_dest_pp:="/data/stmar/287_abvarfq_lmfdb_recomputation/output_pols/av_fq_pol/";
-    file_list:=Split(Pipe("ls " * fld_old_code,"r"));
+    file_list_old:=Split(Pipe("ls " * fld_old_code,"r"));
     
-    tot:=#file_list; perc:=0; num_pol:=0; num_pp:=0;
-    for i->file in file_list do
+    tot:=#file_list_old; perc:=0; num_pol:=0; num_pp:=0;
+    for i->file in file_list_old do
         if Truncate(i*100/tot) gt perc then perc+:=1; printf "%o%% %o %o\n",perc,num_pp,num_pol; end if;
         file_output:=fld_dest_pp * file;
-        lines:=Split(Read(fld_old_code * file));
-        for l in lines do
-            num_pol+:=1;
-            label:=Split(l,":")[1]; // label of form g.q.coeff-N.i.w.j-d.k
-            split:=Split(label,"-.");
-            assert #split eq 9;
-            d:=split[8];
-            assert d in {"1","4","9","25"};
-            if d eq "1" then
-                num_pp+:=1;
-                fprintf file_output,"%o\n",l;
-                //printf "%o\n",l;
-            end if;
-        end for;
+        file_input:=fld_old_code * file;
+        // if file_output exists then it should not contain any non-princ pols.
+        // if so, we delete file_input otherwise we take only the non-princ lines from file_input and 
+        // put them in file_output
+        if OpenTest(file_output,"r") then
+            printf "YES, file exists ... ";
+            lines:=Split(Read(file_output));
+            for l in lines do
+                num_pol+:=1;
+                label:=Split(l,":")[1]; // label of form g.q.coeff-N.i.w.j-d.k
+                split:=Split(label,"-.");
+                assert #split eq 9;
+                d:=split[8];
+                assert d eq "1";
+            end for;
+            Pipe("rm " cat file_input,"r");
+            printf "done\n";
+        else
+            printf "NO, file does not exist ... ";
+            lines:=Split(Read(file_input));
+            for l in lines do
+                num_pol+:=1;
+                label:=Split(l,":")[1]; // label of form g.q.coeff-N.i.w.j-d.k
+                split:=Split(label,"-.");
+                assert #split eq 9;
+                d:=split[8];
+                assert d in {"1","4","9","25"};
+                if d eq "1" then
+                    num_pp+:=1;
+                    fprintf file_output,"%o\n",l;
+                    //printf "%o\n",l;
+                end if;
+            end for;
+            Pipe("rm " cat file_input,"r");
+            printf "done\n";
+        end if;
     end for;
 
     quit;
